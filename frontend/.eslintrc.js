25617374a36d6c59478f684d71afab6f9e785ba9
21503vk <your_email@example.com>
2025-09-30 02:04:14 +0300

    fix(frontend): simplify ESLint config to resolve dependency conflicts
    Removed @vue/typescript/recommended which requires ESLint 9.x
    Using basic config that works with ESLint 8.x
    
    Frontend now starts successfully! рџЋ‰

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -13,7 +13,6 @@ module.exports = {
   extends: [
     'plugin:vue/vue3-recommended',
     'eslint:recommended',
-    '@vue/typescript/recommended',
     'prettier'
   ],
   plugins: ['vue', '@typescript-eslint'],
